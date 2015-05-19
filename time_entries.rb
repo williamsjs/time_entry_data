d66594130ec7a0431419a2245f5be2f283a073ad
Scott Williams <williamsjs1224@gmail.com>
2015-05-18 20:43:09 -0400

    modify time_entries to take task_id

--- time_entries.rb
+++ time_entries.rb
@@ -4,7 +4,7 @@ class TimeEntryMigration < ActiveRecord::Migration
   def change
     create_table :time_entries do |t|
       t.integer :developer_id
-      t.integer :project_id
+      t.integer :task_id
       t.date :work_on
       t.float :length_of_time
     end
